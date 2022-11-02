3e4a45e31a4b36b8a362136de1175b43f3c7d277
a-hagi613 <abdullahi.hagi98@gmail.com>
2022-11-02 14:20:47 -0400

    created role table

---
+++ src/main/java/com/bptn/models/Role.java
@@ -0,0 +1,71 @@
+package com.bptn.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "\"Role\"")
+
+public class Role {
+
+    @Column(name = "\"role\"")
+    private String role;
+
+    @Column(name = "\"company\"")
+    private String company;
+
+    @Column(name = "\"skills\"")
+    private String skills;
+
+    @Column(name = "\"usernameKey\"")
+    private String usernameKey;
+
+    //  constructors
+    public Role() {
+        super();
+    }
+
+    //  parameterized constructors
+    public Role(String role, String company, String skills, String usernameKey) {
+        this.role = role;
+        this.company = company;
+        this.skills = skills;
+        this.usernameKey = usernameKey;
+    }
+
+
+    //    getters
+    public String getRole() {
+        return role;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public String getUsernameKey() {
+        return usernameKey;
+    }
+
+    //    setters
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public void setUsernameKey(String usernameKey) {
+        this.usernameKey = usernameKey;
+    }
+}
