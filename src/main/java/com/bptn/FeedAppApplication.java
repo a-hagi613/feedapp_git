6d168e105dcbde217aff19072187c7ada62735d1
a-hagi613 <abdullahi.hagi98@gmail.com>
2022-11-02 15:22:51 -0400

    application properties fixed

--- src/main/java/com/bptn/FeedAppApplication.java
+++ src/main/java/com/bptn/FeedAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 //import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-
+@SpringBootApplication
+//(exclude = {DataSourceAutoConfiguration.class})
 public class FeedAppApplication {
 
     public static void main(String[] args) {
