83f36f77644a5a7b9f4b1ae49941a5a4f082a00f
a-hagi613 <abdullahi.hagi98@gmail.com>
2022-11-03 11:29:24 -0400

    added repository package and login interface

---
+++ src/main/java/com/bptn/repository/LoginRepository.java
@@ -0,0 +1,31 @@
+/*
+ *
+ *  * Copyright (c) 2019. [Acme Corp]
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in
+ *  * all copies or substantial portions of the Software.
+ *
+ */
+
+package com.bptn.repository;
+
+import com.bptn.models.UserID;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoginRepository extends JpaRepository<UserID, String> {
+    @Query(value = "SELECT * FROM  public.\"UserID\" WHERE username= ?1", nativeQuery = true)
+    List<UserID> getUserById(String username);
+}
