678f846c0b71332bf2af073e96fb1dfb438500a8
a-hagi613 <abdullahi.hagi98@gmail.com>
2022-11-03 14:28:55 -0400

    created LoginController class

---
+++ src/main/java/com/bptn/Controller/LoginController.java
@@ -0,0 +1,42 @@
+/*
+ *
+ *  * Copyright (c) 2019. [Acme Corp]
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in
+ *  * all copies or substantial portions of the Software.
+ *
+ */
+
+package com.bptn.Controller;
+
+import com.bptn.exceptions.InvalidUserCredentialsException;
+import com.bptn.models.UserID;
+import com.bptn.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping("/user/login")
+    public ResponseEntity<?> validateUserCredentials(@RequestBody UserID userCred) throws InvalidUserCredentialsException {
+
+        String loginDetails = loginService.validateUserCredentials(userCred);
+        return new ResponseEntity<>(loginDetails, HttpStatus.OK);
+    }
+
+}
