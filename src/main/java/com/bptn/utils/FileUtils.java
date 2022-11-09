99e21418aec8289b94be6a57b968afb195666806
a-hagi613 <abdullahi.hagi98@gmail.com>
2022-11-09 09:05:24 -0500

    updated utils package

--- src/main/java/com/bptn/utils/FileUtils.java
+++ src/main/java/com/bptn/utils/FileUtils.java
@@ -78,5 +78,59 @@ public class FileUtils {
 
     }
 
+//    public void readFile(Path path) {
+//        if (isFileExist(path)) {
+//            try {
+//                Scanner scanner = new Scanner(new File(path.toUri()));
+//                while (scanner.hasNextLine()) {
+//                    String data = scanner.nextLine();
+//                    LOGGER.info("Data available in file : {}", data);
+//                }
+//            } catch (FileNotFoundException e) {
+//                LOGGER.error("Unable to read the file, cause={}", e.getMessage());
+//            }
+//        }
+//    }
+//
+//    public void deleteFile(Path path) {
+//        if (Files.exists(path)) {
+//            try {
+//                Files.delete(path);
+//                LOGGER.info("File deleted at path={}", path);
+//            } catch (IOException e) {
+//                LOGGER.error("Unable to delete file at path={}, cause={}", path, e.getMessage());
+//            }
+//        } else {
+//            LOGGER.error("File doesn't exist at Path={}", path);
+//        }
+//    }
+//
+//    public void writeFile(Path path) {
+//        if (isFileExist(path)) {
+//            try {
+//                FileWriter writer = new FileWriter(new File(path.toUri()));
+//                writer.write("I'm overwriting the file");
+//                writer.close();
+//                readFile(path);
+//            } catch (IOException e) {
+//                LOGGER.error("Unable to write the file cause={}", e.getMessage());
+//            }
+//        }
+//    }
+//
+//    public void updateFile(Path path) {
+//        if (ifFileExist(path)) {
+//            try {
+//                FileWriter writer = new FileWriter(new File(path.toUri()), true);
+//                writer.write("\r\n");
+//                writer.write("I'm updating the file");
+//                writer.close();
+//                readFile(path);
+//            } catch (IOException e) {
+//                LOGGER.error("Unable to update the file cause={}", e.getMessage());
+//            }
+//        }
+//    }
+
 
 }
