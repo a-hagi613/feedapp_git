fb57091af82eab3a111af720e2ded42ee47b0bd4
a-hagi613 <abdullahi.hagi98@gmail.com>
2022-11-10 09:21:17 -0500

    created FeedMediaRequest class

---
+++ src/main/java/com/bptn/request/FeedMediaRequest.java
@@ -0,0 +1,67 @@
+/*
+ *
+ *  * Copyright (c) 2019. [Acme Corp]
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in
+ *  * all copies or substantial portions of the Software.
+ *
+ */
+
+package com.bptn.request;
+
+public class FeedMediaRequest {
+
+    private String imageName;
+    private String imageSize;
+    private String imageFormat;
+    private String resolution;
+    private String postKey;
+
+    //    getters and setters
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getImageSize() {
+        return imageSize;
+    }
+
+    public void setImageSize(String imageSize) {
+        this.imageSize = imageSize;
+    }
+
+    public String getImageFormat() {
+        return imageFormat;
+    }
+
+    public void setImageFormat(String imageFormat) {
+        this.imageFormat = imageFormat;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    public String getPostKey() {
+        return postKey;
+    }
+
+    public void setPostKey(String postKey) {
+        this.postKey = postKey;
+    }
+}
